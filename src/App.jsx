4365c7ba37ee8fa89c1e760774196ff7b891701d
Alex-gikungu <alexigikungu.012@gmail.com>
2023-08-01 15:54:32 +0300

    created buttons and a single paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import react from 'react'
-
-import './App.css'
+import HomePage from './components/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+<HomePage/>
     </>
   )
 }
 
-export default App
+export default App ;
+
+
+
+
+
+
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/home">
+//           {/* Your Home page component */}
+//         </Route>
+//         <Route path="/products">
+//           {/* Your Products page component */}
+//         </Route>
+//         <Route path="/wishlist">
+//           {/* Your Wishlist page component */}
+//         </Route>
+//         {/* Other routes */}
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+
+// export default 
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+function HomePage() {
+  return (
+    <div className="cover-page">
+      <div className="logo">
+      <img src="1.png"/>
+      </div>
+      
+      <div className="logo_s">
+        <button className="btn_0">KICKOFF MART</button>
+        <p>-Your one stop shop for every sportwear-</p>
+      </div>
+{/* 
+         <div className="btn_1">
+      <button className="btn1" >HOME</button>
+      <button className="btn2">PRODUCTS</button>
+      <button className="btn3">WISHLIST</button>
+      </div> */}
+    </div>
+
+
+
+
+  );
+}
+
+export default HomePage;
+
+
+
+
+
+
+// import React from "react";
+// import { Link } from "react-router-dom"; 
+
+// function HomePage() {
+//   return (
+//     <div className="cover-page">
+//       <div className="logo">
+//         <img src="1.png" alt="Logo" />
+//       </div>
+      
+//       <div className="logo_s">
+//         <button className="btn_0">KICKOFF MART</button>
+//         <p>-Your one stop shop for every sportwear-</p>
+//       </div>
+
+//       <div className="btn_1">
+//         {/* Use Link component from react-router-dom to create navigation links */}
+//         <Link to="/home" className="btn1">HOME</Link>
+//         <Link to="/products" className="btn2">PRODUCTS</Link>
+//         <Link to="/wishlist" className="btn3">WISHLIST</Link>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default HomePage;
