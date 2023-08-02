9d94c6e17c05fa7fe8a3b17177a21dc0339b6726
aluochvivian <aluochviviann@gmail.com>
2023-08-02 08:59:00 +0300

    Footer file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import react from 'react'
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+<Footer/>
     </>
   )
 }
