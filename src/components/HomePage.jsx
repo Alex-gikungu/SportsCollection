42668902862be414bbcaafe8c19dc11bc8a9a4a3
Alex-gikungu <alexigikungu.012@gmail.com>
2023-08-02 23:26:43 +0300

    added a license and updated readme file

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -17,10 +17,6 @@
 //       <button className="btn3">WISHLIST</button>
 //       </div>
 //     </div>
-
-
-
-
 //   );
 // }
 
@@ -31,29 +27,29 @@
 
 
 
-// // import React from "react";
-// // import { Link } from "react-router-dom"; 
+import React from "react";
+import { Link } from "react-router-dom"; 
 
-// // function HomePage() {
-// //   return (
-// //     <div className="cover-page">
-// //       <div className="logo">
-// //         <img src="1.png" alt="Logo" />
-// //       </div>
+function HomePage() {
+  return (
+    <div className="cover-page">
+      <div className="logo">
+        <img src="1.png" alt="Logo" />
+      </div>
       
-// //       <div className="logo_s">
-// //         <button className="btn_0">KICKOFF MART</button>
-// //         <p>-Your one stop shop for every sportwear-</p>
-// //       </div>
+      <div className="logo_s">
+        <button className="btn_0">KICKOFF MART</button>
+        <p>-Your one stop shop for every sportwear-</p>
+      </div>
 
-// //       <div className="btn_1">
-// //         {/* Use Link component from react-router-dom to create navigation links */}
-// //         <Link to="/home" className="btn1">HOME</Link>
-// //         <Link to="/products" className="btn2">PRODUCTS</Link>
-// //         <Link to="/wishlist" className="btn3">WISHLIST</Link>
-// //       </div>
-// //     </div>
-// //   );
-// // }
+      <div className="btn_1">
+        {/* Use Link component from react-router-dom to create navigation links */}
+        <Link to="/home" className="btn1">HOME</Link>
+        <Link to="/products" className="btn2">PRODUCTS</Link>
+        <Link to="/wishlist" className="btn3">WISHLIST</Link>
+      </div>
+    </div>
+  );
+}
 
-// // export default HomePage;
+export default HomePage;
