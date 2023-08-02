a0d5c9284a52da89ef924fe0e6fa3769617b677d
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-08-02 22:39:00 +0300

    Added description of products

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import react from 'react'
-
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Products from "./Products";
+import Description from "./description";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sportsData, setSportsData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/sports")
+      .then((res) => res.json())
+      .then((data) => setSportsData(data));
+  }, []);
 
   return (
-    <>
-
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <h1>SPORTS CENTRE</h1>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Products sportsData={sportsData} />}
+          />
+          <Route
+            path="/description/:sportName/:id"
+            element={<Description sportsData={sportsData} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
